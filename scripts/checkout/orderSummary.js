ff9ea5a059754368f5cbcde3b51ef4db5d99ef73
Eliseo Alcaraz <elipalcaraz@gmail.com>
2024-07-21 00:22:31 +0800

    Considered the weekends as not a part of shipping days

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -3,10 +3,11 @@ import { products, getProduct } from '../../data/products.js';
 import { formatCurrency } from '../utils/money.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 
-import { deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js';
+import { deliveryOptions, getDeliveryOption, calculateDeliveryDate} from '../../data/deliveryOptions.js';
 
 import { renderPaymentSummary } from './paymentSummary.js';
 import { renderCheckoutHeader } from './checkOutHeader.js';
+
 
 export function renderOrderSummary() {
 
@@ -77,13 +78,8 @@ export function renderOrderSummary() {
     let html = '';
 
     deliveryOptions.forEach((deliveryOption) => {
-      const today = dayjs();
-      const deliveryDate = today.add(
-        deliveryOption.deliveryDays, 'days'
-      );
-      const dateString = deliveryDate.format(
-        'dddd, MMMM D'
-      );
+      
+      const dateString = calculateDeliveryDate(deliveryOption);
 
       const priceString = deliveryOption.priceCents === 0
       ? 'FREE' : `$${formatCurrency(deliveryOption.priceCents)} - `;
