aac66106531044337b9c48f27469a7ef47ece4cb
Eliseo Alcaraz <elipalcaraz@gmail.com>
2024-07-25 17:42:24 +0800

    productTest.js

---
+++ tests/data/productTest.js
@@ -0,0 +1,24 @@
+import { getProduct } from '../../data/products.js';
+import { localStorage } from '../../data/cart.js';
+
+describe('test suite: getProduct', () => {
+  beforeEach(() => {
+    spyOn(localStorage, 'setItem');
+
+    spyOn(localStorage, 'getItem').and.callFake(() => {
+      return JSON.stringify([{
+        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+        quantity: 1,
+        deliveryOptionId: '1'
+      }]);
+    });
+    
+    loadFromStorage(); 
+  });
+
+  it('product match', () => {
+    
+    expect(getProduct('e43638ce-6aa0-4b85-b27f-e1d07eb678c6').name).toEqual('Black and Gray Athletic Cotton Socks - 6 Pairs');
+  
+  });
+}); 
