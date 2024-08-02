7ff3503423d382c0198aac53056da2ceba6b3d09
Eliseo Alcaraz <elipalcaraz@gmail.com>
2024-08-02 14:49:42 +0800

    load products from the backend

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,10 +1,13 @@
 import { renderOrderSummary } from './checkout/orderSummary.js';
 import { renderPaymentSummary } from './checkout/paymentSummary.js';
 import { renderCheckoutHeader } from './checkout/checkOutHeader.js';
-//import '../data/cart-class.js';
-import '../data/backend-practice.js';
+// import '../data/cart-class.js';
+// import '../data/backend-practice.js';
+import { loadProducts } from '../data/products.js';
 
-renderOrderSummary();
-renderPaymentSummary();
-renderCheckoutHeader();
+loadProducts(() => {
+  renderOrderSummary();
+  renderPaymentSummary();
+  renderCheckoutHeader();
+});
 
