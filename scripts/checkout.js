2b50479a62420d8c3328c0ef4472032ee09500a4
Eliseo Alcaraz <elipalcaraz@gmail.com>
2024-08-02 17:35:04 +0800

    Learn promises and convert code to promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -4,10 +4,60 @@ import { renderCheckoutHeader } from './checkout/checkOutHeader.js';
 // import '../data/cart-class.js';
 // import '../data/backend-practice.js';
 import { loadProducts } from '../data/products.js';
+import { loadCart } from '../data/cart.js';
 
-loadProducts(() => {
+Promise.all([
+  new Promise((resolve) => {
+    loadProducts(() => {
+      resolve('value1');
+    });
+  }),
+
+  new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  })
+
+]).then((values) => {
+  console.log(values);
   renderOrderSummary();
   renderPaymentSummary();
   renderCheckoutHeader();
 });
+
+
+/*
+new Promise((resolve) => {
+  loadProducts(() => {
+    resolve('value1');
+  });
+
+}).then((value) => {
+  console.log(value);
+
+  return new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  });
+
+}).then(() => {
+  renderOrderSummary();
+  renderPaymentSummary();
+  renderCheckoutHeader();
+});
+*/
+
+
+/*
+loadProducts(() => {
+  loadCart(() => {
+    renderOrderSummary();
+    renderPaymentSummary();
+    renderCheckoutHeader();
+  });
+});
+*/
+
 
